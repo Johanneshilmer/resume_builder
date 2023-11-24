6d0601df11d1ca371acc6e269b1b7f4023a89ea6
johanneshilmer <johanneshilmerson94@gmail.com>
2023-11-24 23:42:01 +0100

    update

--- app/views.py
+++ app/views.py
@@ -4,14 +4,17 @@ from .forms import ResumeForm, EducationForm, ExperienceForm, SkillForm
 
 def create_resume(request):
   if request.method == "POST":
-    form = ResumeForm(request.POST, request.FILES)
-    if form.is_valid():
-      form.save()
+    form1 = ResumeForm(request.POST, request.FILES)
+    form2 = Education(request.POST, prefix="1-")
+    if form1.is_valid() and form2.is_valid():
+      form1.save()
+      form2.save()
       return render(request, "resume.html")
   else:
-    form = ResumeForm()
-  image = Resume.objects.all()
-  return render(request, "home.html", {"form":form, "image":image})
+    form1 = ResumeForm()
+    form2 = Education()
+  return render(request, "home.html", {"form1":form1,
+                                       "form2":form2})
 
 def resume(request):
   data = Resume.objects.all()
