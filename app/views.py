bda4b40b9637149408207a5ab9fe572d6eca348f
johanneshilmer <johanneshilmerson94@gmail.com>
2023-11-25 10:49:34 +0100

    update

--- app/views.py
+++ app/views.py
@@ -3,18 +3,31 @@ from .models import Resume, Education, Experience, Skill
 from .forms import ResumeForm, EducationForm, ExperienceForm, SkillForm
 
 def create_resume(request):
+
   if request.method == "POST":
     form1 = ResumeForm(request.POST, request.FILES)
-    form2 = Education(request.POST, prefix="1-")
-    if form1.is_valid() and form2.is_valid():
+    form2 = EducationForm(request.POST)
+    form3 = ExperienceForm(request.POST)
+    form4 = SkillForm(request.POST)
+    if form1.is_valid() and form2.is_valid() and form3.is_valid() and form4.is_valid():
       form1.save()
       form2.save()
+      form3.save()
+      form4.save()
       return render(request, "resume.html")
   else:
     form1 = ResumeForm()
-    form2 = Education()
-  return render(request, "home.html", {"form1":form1,
-                                       "form2":form2})
+    form2 = EducationForm()
+    form3 = ExperienceForm()
+    form4 = SkillForm()
+  
+  context = {
+    "form1":form1,
+    "form2":form2,
+    "form3":form3,
+    "form4":form4,
+  }
+  return render(request, "home.html", context=context)
 
 def resume(request):
   data = Resume.objects.all()
